5b3c948d99efaab5b9b63e1f92a13697caf056c3
ruud.voost <ruudvoost@gmail.com>
2014-01-14 21:07:19 +0100

    Base created
    index.html, zwiper.less, zwiper.js, and vendor less.js css preprocessor
    added. Ready for lift off!

---
+++ _js/zwiper.js
@@ -0,0 +1,10 @@
+/*
+ * zwiper.js
+ * Created by: Ruud Voost
+ */
+
+function zwiper(){
+    var ready = 'Ready for lift off!';
+    console.log(ready);
+    document.getElementById('title').textContent = ready;
+}
